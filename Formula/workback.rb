fb50813e2899e675195a3ccb7428110d53a1f27d
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2025-05-08 17:18:17 -0700

    chore(workback.rb): update release

--- Formula/workback.rb
+++ Formula/workback.rb
@@ -3,8 +3,8 @@ class Workback < Formula
 
   desc  "An agentic debugging tool that lives in your terminal and helps you debug faster - all through natural language commands."
   homepage "https://hyperdrive.engineering"
-  url "https://files.pythonhosted.org/packages/source/w/workback/workback-0.1.1.tar.gz"
-  sha256 "d43e8083ec8054eb54307b8d81b9517cf0b9751737edd8bd1171ddac0ff9bf1b"
+  url "https://files.pythonhosted.org/packages/source/w/workback/workback-0.1.2.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "All rights reserved"
 
   depends_on "python3"
